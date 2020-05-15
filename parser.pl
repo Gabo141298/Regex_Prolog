9e8a9d05f7c40fdc393ba6b5d3a75f150ded6a64
Gabo Gálvez <gabo141298@gmail.com>
2020-05-14 23:01:12 -0600

    Operador estrella
    Arreglos en el operador estrella. Falta hacer pruebas para comprobar que los automatas se generen bien.

--- parser.pl
+++ parser.pl
@@ -7,27 +7,29 @@ retract_all() :- retractall(estado(X)),
 				 retractall(ultimo_estado(X)),
 				 retractall(transicion(X, Y, Z)).
 
-parse_regex(Regex) :- assert(ultimo_estado(0)), parse(Regex, [0], [0, 0]).
+parse_regex(Regex) :- assert(estado(0)), assert(estado(1)), assert(ultimo_estado(1)), parse(Regex, [[0,1]], [0, 0]).
 
-parse([], _, _).
-parse([')','*'|Xr], [PrimerEstado | EstadosAfuera], [UltimoEstado, PenultimoEstado]):- estrella(PrimerEstado, UltimoEstado, NuevoUltimoEstado), 
-																 	parse(Xr, EstadosAfuera, [NuevoUltimoEstado, UltimoEstado, PenultimoEstado]).
-parse([')'|Xr], [_|EstadosAfuera], UltimosEstados) :- parse(Xr, EstadosAfuera, UltimosEstados).
-parse(['('|Xr], EstadosAfuera, [UltimoEstado, PenultimoEstado]) :- parse(Xr, [UltimoEstado | EstadosAfuera], [UltimoEstado, PenultimoEstado]).
-parse(['|'|Xr], [PrimerEstado|EstadosAfuera], [UltimoEstado, _]) :- parse(Xr, [PrimerEstado|EstadosAfuera], [PrimerEstado, UltimoEstado]).
-parse(['*'|Xr], _, [UltimoEstado, PenultimoEstado]) :- estrella(PenultimoEstado, UltimoEstado, NuevoUltimoEstado), 
-									parse(Xr, EstadosAfuera, [NuevoUltimoEstado, UltimoEstado]).
-parse([X|Xr], EstadosAntesParen, [UltimoEstado, _]) :- nuevo_estado(Trans), crear_transicion(X, UltimoEstado, Trans), parse(Xr, EstadosAntesParen, [Trans, UltimoEstado]).
+parse([], [[_, CierraEstado] | _], [UltimoEstado, _]) :- crear_transicion('¬', UltimoEstado, CierraEstado).
+parse([')','*'|Xr], [[AbreEstado, CierraEstado] | EstadosAfuera], [UltimoEstado, PenultimoEstado]):-
+																	estrella(AbreEstado, CierraEstado, NuevoUltimoEstado), 
+																 	parse(Xr, EstadosAfuera, [NuevoUltimoEstado, CierraEstado]).
+parse([')'|Xr], [[_, CierraEstado]|EstadosAfuera], [UltimoEstado, PenultimoEstado]) :- crear_transicion('¬', UltimoEstado, CierraEstado),
+																	   parse(Xr, EstadosAfuera, [CierraEstado, UltimoEstado]).
+parse(['('|Xr], EstadosAfuera, [UltimoEstado, PenultimoEstado]) :- nuevo_estado(N), parse(Xr, [[UltimoEstado, N] | EstadosAfuera], [UltimoEstado, PenultimoEstado]).
+parse(['|'|Xr], [[AbreEstado, CierraEstado] | EstadosAfuera], [UltimoEstado, _]) :- crear_transicion('¬', UltimoEstado, CierraEstado),
+																					parse(Xr, [[AbreEstado, CierraEstado] | EstadosAfuera], [AbreEstado, UltimoEstado]).
+parse(['*'|Xr], EstadosAfuera, [UltimoEstado, PenultimoEstado]) :- estrella(PenultimoEstado, UltimoEstado, NuevoUltimoEstado), 
+																   parse(Xr, EstadosAfuera, [NuevoUltimoEstado, UltimoEstado]).
+parse([X|Xr], EstadosAntesParen, [UltimoEstado, _]) :- nuevo_estado(Trans), crear_transicion(X, UltimoEstado, Trans), 
+													   parse(Xr, EstadosAntesParen, [Trans, UltimoEstado]).
 
-crear_transicion(Caracter, EstadoInicial, EstadoFinal) :- assert(transicion(EstadoInicial, Caracter, EstadoFinal)), 
-														  write(assert(transicion(EstadoInicial, Caracter, EstadoFinal))).
+crear_transicion(Caracter, EstadoInicial, EstadoFinal) :- transicion(EstadoInicial, Caracter, EstadoFinal).
+crear_transicion(Caracter, EstadoInicial, EstadoFinal) :- assert(transicion(EstadoInicial, Caracter, EstadoFinal)).
 
 % [[q0,nil,q1],[q1,X,q2],[q2,nil,q3],[q0,nil,q3],[q3,nil,q1]]
-estrella(PrimerEstado, UltimoEstado, NuevoUltimoEstado) :- write("estrella\n"),
-														   nuevo_estado(Intermedio),
+estrella(PrimerEstado, UltimoEstado, NuevoUltimoEstado) :- nuevo_estado(Intermedio),
 														   nuevo_estado(NuevoUltimoEstado),
 														   retract_trans(PrimerEstado, L),
-														   write("retract-insert " + a(PrimerEstado, L, Intermedio) + "\n"),
 														   insert_trans(PrimerEstado, L, Intermedio),
 														   crear_transicion('¬', UltimoEstado, NuevoUltimoEstado),
 														   crear_transicion('¬', PrimerEstado, NuevoUltimoEstado),
@@ -36,13 +38,13 @@ estrella(PrimerEstado, UltimoEstado, NuevoUltimoEstado) :- write("estrella\n"),
 devolverse(EstadoFinal, [[E|_]|Xr], EstadoInicial) :- crear_transicion('¬', EstadoFinal, E), devolverse(EstadoFinal, Xr).
 
 % t(Primer, T, E)
-insert_trans(_, [], _). % :- write("termina insert\n").
-insert_trans(Primer, [[E, T] | Xr], Intermedio) :- write("insert:" + a(Primer, ¬, Intermedio) + b(Intermedio, T, E)),
-												   crear_transicion('¬', Primer, Intermedio), crear_transicion(T, Intermedio, E), insert_trans(Primer, Xr, Intermedio).
+insert_trans(_, [], _).
+insert_trans(Primer, [[E, T] | Xr], Intermedio) :- crear_transicion('¬', Primer, Intermedio), crear_transicion(T, Intermedio, E), 
+												   insert_trans(Primer, Xr, Intermedio).
 
 
-retract_trans(X, []) :- \+transicion(X, T, E), write("Después retract_trans\n").
-retract_trans(X, [[E, T]|L1]) :- transicion(X, T, E), write("retract_trans: " + transicion(X, T, E) + "\n"), retract(transicion(X, T, E)), retract_trans(X, L1), write(L1 + "\n").
+retract_trans(X, []) :- \+transicion(X, T, E).
+retract_trans(X, [[E, T]|L1]) :- transicion(X, T, E), retract(transicion(X, T, E)), retract_trans(X, L1).
 
 nuevo_estado(Nuevo) :- ultimo_estado(E), retract(ultimo_estado(E)), Nuevo is E + 1, 
 					   assert(ultimo_estado(Nuevo)), assert(estado(Nuevo)).
