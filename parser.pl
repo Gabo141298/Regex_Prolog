7bebce80e247e2ad3a082e36c378d8e71f7e98df
Gabo Gálvez <gabo141298@gmail.com>
2020-05-18 21:42:10 -0600

    Arreglos en la generación del automata

--- parser.pl
+++ parser.pl
@@ -10,9 +10,10 @@
 regex_match(Regex, Hilera, Match) :- retract_all(),
 									 string_chars(Regex, RegexChars),
 									 parse_regex(RegexChars),
+									 %listing(transicion),
 									 string_chars(Hilera, HileraChars),
 									 estado_inicial(X),
-									 accept_string(HileraChars, MatchChars, X),
+									 accept_string(HileraChars, MatchChars, X, []),
 									 !, estado_aceptado,
 									 string_chars(Match, MatchChars).
 
@@ -24,35 +25,41 @@ retract_all() :- retractall(estado(_)),
 				 retractall(transicion(_, _, _)),
 				 retractall(estado_aceptado).
 
-% accept_string/3(+Hilera, -Match, +Actual):
+% accept_string/4(+Hilera, -Match, +Actual, +UltimosRecorridosConNil):
 %				Usa las reglas que forman al automata para verificar si el automata acepta a la hilera.
 %				Parámetros:
 %					Hilera: lista con cada caracter de la hilera.
 %					Match: lista donde se devuelve la subhilera que es aceptada por el automata.
 %					Actual: estado actual que se está recorriendo en el automata.
-accept_string(_, [], Actual) :- estado_aceptacion(Actual), assert(estado_aceptado), !.
-accept_string([], Match, Actual) :- findall(Y, transicion(Actual, '¬', Y), L),
-									explore_transitions([], '¬', Actual, L, Match).
-accept_string([], [], _).
-accept_string([X|Xr], Match, Actual) :- findall(Y, transicion(Actual, X, Y), L1),
+%					UltimosRecorridosConNil: lista con los últimos estados que se recorrieron con nil, 
+%											para evitar recorrer los mismos estados y enciclar el programa.
+accept_string(_, [], Actual, _) :- estado_aceptacion(Actual), assert(estado_aceptado), !.
+accept_string([], Match, Actual, RecorridosNil) :- findall(Y, transicion(Actual, '¬', Y), L),
+									explore_nil_transitions([], Actual, L, Match, RecorridosNil).
+accept_string([], [], _, _).
+accept_string([X|Xr], Match, Actual, RecorridosNil) :- findall(Y, transicion(Actual, X, Y), L1),
 										findall(Z, transicion(Actual, '¬', Z), L2),
-										(explore_transitions([X|Xr], X, Actual, L1, Match);
-										explore_transitions([X|Xr], '¬', Actual, L2, Match)).
-accept_string([_|Xr], Match, _) :- estado_inicial(I), accept_string(Xr, Match, I).
+										(explore_transitions([X|Xr], Actual, L1, Match);
+										explore_nil_transitions([X|Xr], Actual, L2, Match, RecorridosNil)).
+accept_string([_|Xr], Match, _, _) :- estado_inicial(I), accept_string(Xr, Match, I, []), !.
 
-% explore_transitions/5(+Hilera, +CaracterTransicion, +Actual, +Transiciones, -Match):
+% explore_transitions/6(+Hilera, +Actual, +Transiciones, -Match):
 %				Explora todas las transiciones que hay desde el estado actual del automata usando un caracter de transición en particular.
 %				Parámetros:
 %					Hilera: lista con caracteres de la hilera.
-%					CaracterTransicion: caracter con el que se está haciendo la transición. Principalmente usado para verificar si es un nil (¬).
 %					Actual: estado actual que se está recorriendo en el automata.
 %					Transiciones: lista con los posibles estados a los que se puede pasar desde el estado actual, con el caracter de transición.
 %					Match: lista donde se devuelve la subhilera que es aceptada por el automata.
-explore_transitions(_, _, Actual, _, []) :- estado_aceptacion(Actual), !.
-explore_transitions(Hilera, '¬', _, [Y|_], Match) :- accept_string(Hilera, Match, Y), estado_aceptado.
-explore_transitions(Hilera, '¬', Actual, [_|Yr], Match) :- explore_transitions(Hilera, '¬', Actual, Yr, Match), !.
-explore_transitions([X|Xr], _, _, [Y|_], [X|Match]) :- accept_string(Xr, Match, Y), estado_aceptado.
-explore_transitions([X|Xr], _, Actual, [_|Yr], [X|Match]) :- explore_transitions([X|Xr], X, Actual, Yr, Match).
+explore_transitions(_, Actual, _, []) :- estado_aceptacion(Actual), !.
+explore_transitions([X|Xr], Actual, [Y|_], [X|Match]) :- %write([X|Xr] + Actual + Y + "\n"), 
+														accept_string(Xr, Match, Y, []), estado_aceptado.
+explore_transitions([X|Xr], Actual, [_|Yr], [X|Match]) :- explore_transitions([X|Xr], Actual, Yr, Match).
+
+explore_nil_transitions(_, Actual, _, [], _) :- estado_aceptacion(Actual), !.
+explore_nil_transitions(Hilera, Actual, [Y|_], Match, RecorridosNil) :- %write(Hilera + Actual + Y + RecorridosNil + "nil\n"), 
+																		\+member(Y, RecorridosNil), 
+																		accept_string(Hilera, Match, Y, [Y|RecorridosNil]), estado_aceptado.
+explore_nil_transitions(Hilera, Actual, [_|Yr], Match, RecorridosNil) :- explore_nil_transitions(Hilera, Actual, Yr, Match, RecorridosNil).
 
 % parse_regex/1(+Regex):
 %				parsea la expresión regular, para generar las reglas para los estados y las transiciones.
@@ -69,17 +76,31 @@ parse_regex(Regex) :- assert(estado(0)), assert(estado_inicial(0)), assert(estad
 %					EstadosAfueraParentesis: lista con los pares de estados que encierran a la expresión que está entre paréntesis.
 %					UltimosEstados: lista con los dos estados anteriores en la expresión, con orden [Ultimo, Penultimo].
 parse([], [[_, CierraEstado] | _], [UltimoEstado, _]) :- crear_transicion('¬', UltimoEstado, CierraEstado).
-parse([')','*'|Xr], [[AbreEstado, CierraEstado] | EstadosAfuera], _):-
+parse([')','*'|Xr], [[AbreEstado, CierraEstado] | EstadosAfuera], [UltimoEstado|_]):- 
+																	crear_transicion('¬', UltimoEstado, CierraEstado),
+																	%listing(transicion), write(AbreEstado + CierraEstado + NuevoUltimoEstado + "\n"),
 																	estrella(AbreEstado, CierraEstado, NuevoUltimoEstado), 
 																 	parse(Xr, EstadosAfuera, [NuevoUltimoEstado, CierraEstado]).
 parse([')'|Xr], [[_, CierraEstado]|EstadosAfuera], [UltimoEstado, _]) :- crear_transicion('¬', UltimoEstado, CierraEstado),
 																	   parse(Xr, EstadosAfuera, [CierraEstado, UltimoEstado]).
-parse(['('|Xr], EstadosAfuera, [UltimoEstado, PenultimoEstado]) :- nuevo_estado(N), parse(Xr, [[UltimoEstado, N] | EstadosAfuera], [UltimoEstado, PenultimoEstado]).
-parse(['|'|Xr], [[AbreEstado, CierraEstado] | EstadosAfuera], [UltimoEstado, _]) :- crear_transicion('¬', UltimoEstado, CierraEstado),
-																					parse(Xr, [[AbreEstado, CierraEstado] | EstadosAfuera], [AbreEstado, UltimoEstado]).
-parse(['*'|Xr], EstadosAfuera, [UltimoEstado, PenultimoEstado]) :- estrella(PenultimoEstado, UltimoEstado, NuevoUltimoEstado), 
+parse(['('|Xr], [[AbreEstado, CierraEstado] | EstadosAfuera], [UltimoEstado, _]) :- nuevo_estado(Nuevo),
+													 crear_transicion('¬', AbreEstado, Nuevo),
+													 nuevo_estado(NuevoCierre), 
+													 parse(Xr, [[UltimoEstado, NuevoCierre], [AbreEstado, CierraEstado] | EstadosAfuera], [Nuevo, UltimoEstado]).
+parse(['|'|Xr], [[AbreEstado, CierraEstado] | EstadosAfuera], [UltimoEstado, _]) :- %write([AbreEstado, CierraEstado] + UltimoEstado + "\n"),
+																					nuevo_estado(Nuevo),
+																					crear_transicion('¬', AbreEstado, Nuevo),
+																					%listing(transicion),
+																					crear_transicion('¬', UltimoEstado, CierraEstado),
+																					parse(Xr, [[AbreEstado, CierraEstado] | EstadosAfuera], [Nuevo, AbreEstado]).
+parse(['*'|Xr], EstadosAfuera, [UltimoEstado, PenultimoEstado]) :- %write("estrella sola: " + [UltimoEstado, PenultimoEstado]),
+																   %listing(transicion),
+																   estrella(PenultimoEstado, UltimoEstado, NuevoUltimoEstado),
+																   %listing(transicion),
 																   parse(Xr, EstadosAfuera, [NuevoUltimoEstado, UltimoEstado]).
-parse([X|Xr], EstadosAntesParen, [UltimoEstado, _]) :- nuevo_estado(Trans), crear_transicion(X, UltimoEstado, Trans), 
+parse([X|Xr], EstadosAntesParen, [UltimoEstado, _]) :- %write("Carac: " + X + EstadosAntesParen + UltimoEstado + "\n"),
+													   nuevo_estado(Trans), crear_transicion(X, UltimoEstado, Trans), 
+													   %listing(transicion),
 													   parse(Xr, EstadosAntesParen, [Trans, UltimoEstado]).
 
 % crear_transicion/3(+Caracter, +EstadoInicial, +EstadoFinal):
@@ -107,7 +128,7 @@ estrella(PrimerEstado, UltimoEstado, NuevoUltimoEstado) :- nuevo_estado(Intermed
 														   crear_transicion('¬', UltimoEstado, NuevoUltimoEstado),
 														   crear_transicion('¬', PrimerEstado, NuevoUltimoEstado),
 														   crear_transicion('¬', NuevoUltimoEstado, Intermedio).
-
+														   
 % insert_trans/3(+PrimerEstado, +EstadosSiguientes, +Intermedio):
 %				Inserta un estado en medio de un primer estado y todos los posibles estados a los que puede llegar directamente.
 %				Parámetros:
